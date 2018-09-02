18d24897cad1127fadb0dad6551d88aab047b559
Adrian Mrowka <varnaxes0@gmail.com>
2018-09-02 21:49:45 +0200

    Added reversing characters

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
+// DISCLAIMER: All of the lines MUST have the same amount of characters - you need to fill 
+
 #include <iostream>
 #include <fstream>
 #include <string>
 
 using namespace std;
 
-int reverse_line(string & line);
+void reverse_line(string & line);
+void reverse_characters(string & line);
+void mirror(char & x);
 
 int main()
 {
@@ -18,20 +22,75 @@ int main()
         while (getline(file_from, line)) 
         {
             reverse_line(line);
+            reverse_characters(line);
             file_to << line;
             file_to << '\n';
         }
         file_from.close();
         file_to.close();
+        cout << "Reverse completed.\n";
     }
     else
         cout << "Can't open file.\n";
 }
 
-int reverse_line(string & line) 
+void reverse_line(string & line) 
 {
     int len = line.length();
  
     for (int i = 0; i < len / 2; ++i)
         swap(line[i], line[len-1-i]);
+}
+
+void reverse_characters(string & line)
+{
+    int len = line.length();
+
+    for (int i = 0; i < len; ++i)
+        mirror(line[i]);
+}
+
+void mirror (char & x) // THIS IS REALLY POOR CODE, IF YOU HAVE ANY IDEAS, TELL ME
+{
+    switch(x)
+    {
+    case '/':
+        x = '\\';
+        break;
+    case '\\':
+        x = '/';
+        break;
+    case '(':
+        x = ')';
+        break;
+    case ')':
+        x = '(';
+        break;
+    case '<':
+        x = '>';
+        break;     
+    case '>':
+        x = '<';
+        break;
+    case '[':
+        x = ']';
+        break;
+    case ']':
+        x = '[';
+        break;
+    case '{':
+        x = '}';
+        break;
+    case '}':
+        x = '{';
+        break;
+    case 'b':
+        x = 'd';
+        break;
+    case 'd':
+        x = 'b';
+        break;
+    default:
+        break;       
+    }
 }
